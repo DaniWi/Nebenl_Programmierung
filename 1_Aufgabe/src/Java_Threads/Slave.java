01656c4a15221461d9e89c53a6b5cd4db5141b97
Pati <patrick.deutsch@outlook.com>
2015-10-23 10:07:35 +0200

    final

--- 1_Aufgabe/src/Java_Threads/Slave.java
+++ 1_Aufgabe/src/Java_Threads/Slave.java
@@ -8,6 +8,13 @@ public class Slave implements Runnable {
 		//print current thread id
 		System.out.println("Thread " + Thread.currentThread().getId() + " is running");
 		
+		try {
+			Thread.sleep(1000000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
